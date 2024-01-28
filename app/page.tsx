b0ad08298892740c69e45aea6a940a2856bcaa33
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-01-28 14:39:02 +0100

    Added homePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 import Experience from "@/components/Experience";
+import HomePage from "@/components/HomePage";
 import Main from "@/components/Main";
 import Header from "@/components/Navigation/Header";
 import Project from "@/components/Projects/Project";
-
 const Home = () => {
     return (
         <div>
-            <div className="w-[80%] z-20 justify-between mx-auto px-12 h-full items-center  flex">
-                <Header />
-                <main className="overflow-y-auto h-screen  ml-auto w-[47%] mr-12  text-slate-200">
-                    <Main />
-                    <Experience />
-                </main>
-            </div>
+            <HomePage>
+                <div className="w-[80%] z-20 justify-between mx-auto px-12 h-full items-center  flex">
+                    <Header />
+                    <main className="overflow-y-auto h-screen  ml-auto w-[47%] mr-12  text-slate-200">
+                        <Main />
+                        <Experience />
+                        <Project />
+                    </main>
+                </div>
+            </HomePage>
         </div>
     );
 };
