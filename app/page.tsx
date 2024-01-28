4021686b0b5a34729a547425870acd4b018183e0
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-01-28 14:17:39 +0100

    Fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import Experience from "@/components/Experience";
+import HomePage from "@/components/HomePage";
+import Main from "@/components/Main";
+import Header from "@/components/Navigation/Header";
+import Project from "@/components/Projects/Project";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  );
-}
+
+const Home = () => {
+    return (
+        <div>
+            <HomePage>
+                <div className="w-[80%] z-20 justify-between mx-auto px-12 h-full items-center  flex">
+                    <Header/>
+                    <main className="overflow-y-auto h-screen  ml-auto w-[47%] mr-12  text-slate-200">
+                        <Main />
+                        <Experience />
+                        <Project />
+                    </main>
+                </div>
+            </HomePage>
+        </div>
+    );
+};
+
+export default Home;
