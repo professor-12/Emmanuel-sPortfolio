63d4e557360fe79337fa7cfaadb16c936b35cc68
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-08-20 19:44:38 +0100

    Update changes

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Project = async () => {
     const project = await getProject();
     return (
-        <div className="space-y-3 pt-12" id="project">
+        <div className="space-y-3 pt-12 overflow-hidden" id="project">
             <div className="sticky z-[20]  p-2 top-0 backdrop-blur-md text-xl py-6">
                 Project
             </div>
@@ -19,7 +19,7 @@ const Project = async () => {
                             target="_blank"
                             className="flex p-4 md:px-8 hover:bg-[#17274B] rounded-md justify-between gap-8 items-start"
                         >
-                            <div className="w-36">
+                            <div className="lg:w-36">
                                 <Image
                                     className="object-cover min-w-32 m-0 rounded ring-slate-800 ring-[1.5px]"
                                     src={
@@ -39,10 +39,11 @@ const Project = async () => {
                         </Link>
                         <button className="text-sm absolute right-12 bottom-0 bg-violet-500/50 rounded-full my-3 p-1 px-3">
                             <a
+                                target="_blank"
                                 href={site?.build_settings.repo_url ?? ""}
                                 className="text-xs"
                             >
-                                GitHub_URL
+                                Source code
                             </a>
                         </button>
                     </div>
